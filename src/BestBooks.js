117bfd8ea488e02c1eb79e574617a8560170c69b
Harvey Francois <har.francois@gmail.com>
2022-02-28 21:56:34 -0800

    oops i did it again

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import axios from 'axios';
+
+let SERVER = process.env.REACT_APP_SERVER
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -10,16 +13,36 @@ class BestBooks extends React.Component {
 
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
 
+  getBooks = async () => {
+    try {
+      // remember we can grab with a  parameter as well 
+      // let results = await axios(`${SERVER}/cats?location=Bremerton`);
+      let email = `reymercado.usa@gmail.com`;
+      let results = await axios(`${SERVER}/books?email=${email}`);
+      this.setState({
+        books: results.data
+      })
+    } catch(error){
+      console.log('we have an error: ', error.response.data)
+    }
+  }
+  componentDidMount(){
+    this.getBooks();
+  }
+
   render() {
 
     /* TODO: render user's books in a Carousel */
-
+    let books = this.state.books.map((book, idx) => (
+      <p key={idx+book.title} >{book.title} is {book.email}</p>
+    ))
+    console.log(books);
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        {this.state.books.length > 0 ? (
+          <p>{books}</p>
         ) : (
           <h3>No Books Found :(</h3>
         )}
