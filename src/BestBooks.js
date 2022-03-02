231aaa38877c0683ee97b3c214ebed8d352f09e7
Harvey Francois <har.francois@gmail.com>
2022-03-01 16:03:14 -0800

    work in progress

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import axios from 'axios';
+import { Carousel } from 'react-bootstrap';
 
-let SERVER = process.env.REACT_APP_SERVER
-
+let SERVER = process.env.REACT_APP_SERVER;
+console.log('hello world');
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: []
-    }
+    };
   }
 
   /* TODO: Make a GET request to your API to fetch books for the logged in user  */
@@ -17,37 +18,47 @@ class BestBooks extends React.Component {
     try {
       // remember we can grab with a  parameter as well 
       // let results = await axios(`${SERVER}/cats?location=Bremerton`);
-      let email = `reymercado.usa@gmail.com`;
-      let results = await axios(`${SERVER}/books?email=${email}`);
+      // let url = `${SERVER}/books?email=${this.props.user.email}`;
+
+      let results = await axios(`${SERVER}/books?email=${this.props.email}`);
       this.setState({
         books: results.data
-      })
-    } catch(error){
-      console.log('we have an error: ', error.response.data)
+      });
+      console.log('proof of life');
+    } catch (error) {
+      console.log('we have an error: ', error.message);
     }
-  }
-  componentDidMount(){
+  };
+  componentDidMount() {
     this.getBooks();
   }
 
   render() {
 
     /* TODO: render user's books in a Carousel */
-    let books = this.state.books.map((book, idx) => (
-      <p key={idx+book.title} >{book.title} is {book.email}</p>
-    ))
+    // let books = this.state.books.map((book, idx) => (
+    //   <p key={idx + book.title} >{book.title} is {book.email}</p>
+    // ));
     console.log(books);
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
         {this.state.books.length > 0 ? (
-          <p>{books}</p>
+          <Carousel>
+            {this.state.books.map((book, idx) => (
+              <Carousel.Item className='h-100'
+                key={idx}>
+                <Carousel.Caption>
+                  <h1>{book.title}</h1>
+                  <h2>{book.description}</h2>
+                </Carousel.Caption>
+              </Carousel.Item>))}
+          </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
       </>
-    )
+    );
   }
 }
 
